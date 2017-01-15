db18c8efc7c91f3022a27ab6bfbbce83503ce96b
翁晓俊 <wengxj@sdc.icbc.com.cn>
2017-01-15 15:15:33 +0800

    优化ServiceProvider

--- dds-rpc/src/main/java/com/icbc/dds/rpc/service/ServiceProvider.java
+++ dds-rpc/src/main/java/com/icbc/dds/rpc/service/ServiceProvider.java
@@ -3,19 +3,31 @@ package com.icbc.dds.rpc.service;
 import java.util.Iterator;
 import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by kfzx-wengxj on 14/01/2017.
  */
 public class ServiceProvider {
+    private static ConcurrentMap<Class, Object> instanceMap = new ConcurrentHashMap<Class, Object>();
+
     public static <T> T get(Class<T> clazz, Class<? extends T> defaultClass) throws IllegalAccessException, InstantiationException {
+        Object instance = instanceMap.get(clazz);
+        if (instance != null) return (T) instance;
         try {
             ServiceLoader<T> loader = ServiceLoader.load(clazz);
             Iterator<T> iterator = loader.iterator();
-            return iterator.next();
+            instance = iterator.next();
         } catch (ServiceConfigurationError e) {
             // TODO: 15/01/2017 记录日志
-            return defaultClass.newInstance();
+            instance = defaultClass.newInstance();
         }
+        instanceMap.put(clazz, instance);
+        return (T) instance;
+    }
+
+    public static void reset() {
+        instanceMap.clear();
     }
 }
--- dds-rpc/src/test/java/com/icbc/dds/rpc/service/ServiceProviderTest.java
+++ dds-rpc/src/test/java/com/icbc/dds/rpc/service/ServiceProviderTest.java
@@ -1,6 +1,7 @@
 package com.icbc.dds.rpc.service;
 
 import com.icbc.dds.api.RegistryClient;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +10,11 @@ import static org.junit.Assert.*;
  * Created by kfzx-wengxj on 15/01/2017.
  */
 public class ServiceProviderTest {
+    @Before
+    public void clearCache() {
+        ServiceProvider.reset();
+    }
+
     @Test
     public void registryClientServiceNotFoundThenReturnDefaultRegistryClient() throws InstantiationException, IllegalAccessException {
         RegistryClient client = ServiceProvider.get(RegistryClient.class, DefaultRegistryClient.class);
@@ -19,5 +25,12 @@ public class ServiceProviderTest {
     public void exampleServiceShouldReturnExampleServiceProvider() throws InstantiationException, IllegalAccessException {
         ExampleService exampleService = ServiceProvider.get(ExampleService.class, DefaultExampleServiceProvider.class);
         assertTrue(ExampleServiceProvider.class.isInstance(exampleService));
+    }
+
+    @Test
+    public void getOneServiceTwiceShouldReturnTheSameInstance() throws InstantiationException, IllegalAccessException {
+        ExampleService exampleService1 = ServiceProvider.get(ExampleService.class, DefaultExampleServiceProvider.class);
+        ExampleService exampleService2 = ServiceProvider.get(ExampleService.class, DefaultExampleServiceProvider.class);
+        assertSame(exampleService1, exampleService2);
     }
 }
