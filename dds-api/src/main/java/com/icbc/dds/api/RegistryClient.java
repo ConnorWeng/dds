659008b02b760cf8427098b5d1216fc77fd8ca56
翁晓俊 <wengxj@sdc.icbc.com.cn>
2017-01-15 15:54:06 +0800

    调整api接口

--- dds-api/src/main/java/com/icbc/dds/api/RegistryClient.java
+++ dds-api/src/main/java/com/icbc/dds/api/RegistryClient.java
@@ -5,10 +5,6 @@ import com.icbc.dds.api.pojo.InstanceInfo;
 /**
  * Created by kfzx-wengxj on 14/01/2017.
  */
-public abstract class RegistryClient {
-    public abstract InstanceInfo getInstanceByAppName(String appName);
-
-    public InstanceInfo getInstance(String ipAddr, int port) {
-        return new InstanceInfo(ipAddr, port);
-    }
+public interface RegistryClient {
+    InstanceInfo getInstanceByAppName(String appName);
 }
--- dds-rpc/src/main/java/com/icbc/dds/rpc/service/DefaultRegistryClient.java
+++ dds-rpc/src/main/java/com/icbc/dds/rpc/service/DefaultRegistryClient.java
@@ -6,7 +6,7 @@ import com.icbc.dds.api.pojo.InstanceInfo;
 /**
  * Created by kfzx-wengxj on 15/01/2017.
  */
-public class DefaultRegistryClient extends RegistryClient {
+public class DefaultRegistryClient implements RegistryClient {
     @Override
     public InstanceInfo getInstanceByAppName(String appName) {
         return null;
