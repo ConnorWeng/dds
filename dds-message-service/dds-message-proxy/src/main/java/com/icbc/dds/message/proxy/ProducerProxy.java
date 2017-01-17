8c8bdf6e7ee2d8a47fcabea1520a82f8eca41217
翁晓俊 <wengxj@sdc.icbc.com.cn>
2017-01-17 15:38:10 +0800

    新增message proxy示例代码

---
+++ dds-message-service/dds-message-proxy/src/main/java/com/icbc/dds/message/proxy/ProducerProxy.java
@@ -0,0 +1,14 @@
+package com.icbc.dds.message.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kfzx-wengxj on 17/01/2017.
+ */
+@SpringBootApplication
+public class ProducerProxy {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerProxy.class, args);
+    }
+}
