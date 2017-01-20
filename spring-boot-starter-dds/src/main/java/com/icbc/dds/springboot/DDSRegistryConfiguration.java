6e3255fbd7144271ca599af666af3c28098028fc
翁晓俊 <wengxj@sdc.icbc.com.cn>
2017-01-20 14:07:42 +0800

    自动完成dds注册

--- spring-boot-starter-dds/src/main/java/com/icbc/dds/springboot/DDSRegistryConfiguration.java
+++ spring-boot-starter-dds/src/main/java/com/icbc/dds/springboot/DDSRegistryConfiguration.java
@@ -1,6 +1,9 @@
 package com.icbc.dds.springboot;
 
 import com.icbc.dds.api.RegistryClient;
+import com.icbc.dds.registry.client.DDSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DDSRegistryConfiguration implements SmartLifecycle {
+    private final static Logger logger = LoggerFactory.getLogger(DDSRegistryConfiguration.class);
     private boolean isRunning = false;
 
     @Autowired
@@ -18,8 +22,7 @@ public class DDSRegistryConfiguration implements SmartLifecycle {
 
     @Bean
     public RegistryClient registryClient() {
-        // TODO: 17/01/2017 返回具体实现
-        return null;
+        return new DDSClient();
     }
 
     @Override
@@ -29,16 +32,19 @@ public class DDSRegistryConfiguration implements SmartLifecycle {
 
     @Override
     public void stop(Runnable callback) {
+        registryClient.deRegister();
         isRunning = false;
     }
 
     @Override
     public void start() {
+        registryClient.register();
         isRunning = true;
     }
 
     @Override
     public void stop() {
+        registryClient.deRegister();
         isRunning = false;
     }
 
